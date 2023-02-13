5404f7bc1995aa7b60e1785e695d37e578fa519b
Cindyt37 <113190202+Cindyt37@users.noreply.github.com>
2023-02-13 10:16:41 -0500

    .

--- create-task/js/main.js
+++ create-task/js/main.js
@@ -1,4 +1,32 @@
 import "../styles/style.css";
+import "./dom";
 
 const URL_brawlers = "https://api.brawlapi.com/v1/brawlers";
 const URL_events = "https://api.brawlapi.com/v1/events";
+
+async function getBrawlers(URL_brawlers) {
+  try {
+    const response = await fetch(URL_brawlers);
+    const data = await response.json(); //makes the data into JSON object so we ca use
+    function displayBrawlers() {
+      data.list.forEach((brawler) => {
+        document.getElementById("api-output").insertAdjacentHTML("afterbegin"),
+          `<div class="brawler-card" id="${brawler.name}">         
+            <div class="brawler-imgBox">           
+              <img class="brawler-img" src="${brawler.imageUrl}" alt="${brawler.name}"/>
+            </div>         
+            <div class="brawler-descriptions">           
+              <h3 class="brawler-name">${brawler.name}</h3>           
+              <h4 class="brawler-rarity">${brawler.rarity.name}</h4>           
+              <h3 class="brawler-description">${brawler.description}</h3>         
+            </div>       
+          </div>`;
+      });
+    }
+    displayBrawlers();
+  } catch (error) {
+    console.log(error);
+    alert("An error occured.");
+  }
+}
+getBrawlers(URL_brawlers);
