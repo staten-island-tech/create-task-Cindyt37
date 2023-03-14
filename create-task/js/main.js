e7cc24e3a4bf6a5e1c3dfcaa4d691882688f68a5
Cindyt37 <113190202+Cindyt37@users.noreply.github.com>
2023-03-14 09:35:35 -0400

    Update main.js

--- create-task/js/main.js
+++ create-task/js/main.js
@@ -18,8 +18,8 @@ async function getBrawlers(URL_brawlers) {
               <img class="brawler-img" src="${brawler.imageUrl}" alt="${brawler.name}"/>
             </div>         
             <div class="brawler-descriptions">           
-              <h3 class="brawler-name">${brawler.name}</h3>           
-              <h4 class="brawler-rarity" id=${brawler.rarity.name}>${brawler.rarity.name}</h4>           
+              <h1 class="brawler-name">${brawler.name}</h1>           
+              <h2 class="brawler-rarity" id=${brawler.rarity.name}>${brawler.rarity.name}</h2>           
               <h3 class="brawler-description"> ${brawler.description}</h3>
               <h3 class="brawler-id">Brawler ID: ${brawler.id}</h3>
               <h4 class="id-info">To learn more about each brawler, use the ID above to search for the brawlers.</h4>
